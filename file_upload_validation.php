df2b77c9902fb697af4c709bc208f9b50669a2f2
Aijaz Hussain Naqvi <aijaznaqvi@hotmail.com>
2021-05-11 16:51:49 -0400

    Update file_upload_validation.php

--- file_upload_validation.php
+++ file_upload_validation.php
@@ -1,12 +1,10 @@
 
-    /**
-     * Added upload_max_filesize and post_max_size in admin htaccess file and added validation on the size of post request data and uploaded files.
-     */
     public function validateMassUpload ()
     {
         if ($this->getRequest()->isMethod(sfWebRequest::POST)) {
-            if ($_SERVER['CONTENT_LENGTH'] > Util::convertPHPSizeToBytes(ini_get('post_max_size'))) {
-                cgMessager::getInstance()->addError('The data exceeds the maximum size.');
+            $post_max_size = ini_get('post_max_size');
+            if ($_SERVER['CONTENT_LENGTH'] > Util::convertPHPSizeToBytes($post_max_size)) {
+                cgMessager::getInstance()->addError("The data exceeds the maximum size of $post_max_size.");
                 return false;
             } elseif (!$this->csrfTokenIsValid()) {
                 cgMessager::getInstance()->addError('Invalid CSRF Token');
@@ -15,8 +13,9 @@
                 for ($i = 1; $i <= sfConfig::get('app_sfAssetsLibrary_mass_upload_size', 5); $i++) {
                     $filename = $this->getRequest()->getFileName('files[' . $i . ']');
                     $filesize = $this->getRequest()->getFileSize('files[' . $i . ']');
+                    $upload_max_filesize = ini_get('upload_max_filesize');
                     if ($filesize > Util::getMaximumFileUploadSize() || !empty($filename) && $filesize === 0) {
-                        cgMessager::getInstance()->addError('The uploaded file exceeds the maximum size.');
+                        cgMessager::getInstance()->addError("The uploaded file exceeds the maximum size of $upload_max_filesize.");
                         return false;
                     }
                 }
